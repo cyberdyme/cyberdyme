7ded26b58ccd1c10563752f673a032768eaa2ad1
Girish Thanki <girish.thanki@gmail.com>
2020-01-14 22:02:50 +0000

    Using pixi js code

--- src/app/components/canvas/canvas.component.ts
+++ src/app/components/canvas/canvas.component.ts
@@ -1,10 +1,57 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-  AfterViewInit
-} from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as PIXI from 'pixi.js';
+
+class GradientResource extends PIXI.resources.Resource {
+  constructor() {
+    // pass width and height. (0,0) if we dont know yet
+    // gradient needs only 1 pixel height
+    super(256, 1);
+  }
+
+  upload(renderer, baseTexture, glTexture) {
+    const { width } = this; // default size or from baseTexture?
+    const { height } = this; // your choice.
+
+    // temporary canvas, we dont need it after texture is uploaded to GPU
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+
+    const ctx = canvas.getContext('2d');
+
+    const grd = ctx.createLinearGradient(0, 0, width, 0);
+    grd.addColorStop(0, 'rgba(255, 255, 255, 0.0)');
+    grd.addColorStop(0.3, 'cyan');
+    grd.addColorStop(0.7, 'red');
+    grd.addColorStop(1, 'green');
+
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, width, height);
+
+    // This info ios usseful if upload happens second time
+    // Some people use that to track used memory
+    glTexture.width = width;
+    glTexture.height = height;
+
+    // PURE WEBGL CALLS - that's what its all about.
+    // PixiJS cant wrap all that API, we give you acceess to it!
+    const { gl } = renderer;
+    gl.pixelStorei(
+      gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL,
+      baseTexture.premultiplyAlpha
+    );
+    gl.texImage2D(
+      baseTexture.target,
+      0,
+      baseTexture.format,
+      baseTexture.format,
+      baseTexture.type,
+      canvas
+    );
+
+    return true;
+  }
+}
 
 @Component({
   selector: 'app-canvas',
@@ -12,247 +59,36 @@ import {
   styleUrls: ['./canvas.component.scss']
 })
 export class CanvasComponent implements AfterViewInit {
+  @ViewChild('canvasContainer') canvasContainer: ElementRef;
 
-  constructor() {}
-;
-  private X = new Array(600);
-  private Y = new Array(600);
-  private C = new Array(600);
-  /** Template reference to the canvas element */
-  @ViewChild('canvasEl') canvasEl: ElementRef;
-
-  /** Canvas 2d context */
-  private context: CanvasRenderingContext2D;
-  private canvas: HTMLCanvasElement;
+  public app: any;
+  constructor() {
+    this.app = new PIXI.Application({
+      width: 1200,
+      height: 60,
+      backgroundColor: 0xff0000
+    });
+  }
 
   ngAfterViewInit() {
-    this.canvas = this.canvasEl.nativeElement as HTMLCanvasElement;
-    this.context = (this.canvasEl
-      .nativeElement as HTMLCanvasElement).getContext('2d');
+    this.canvasContainer.nativeElement.appendChild(this.app.view);
 
-    const w1 = this.canvas.width - 2;
-    const h1 = this.canvas.height - 2;
-    for (let i = 0; i < 500; i++) {
-      this.X[i] = this.randgp(w1);
-      this.Y[i] = this.randgp(h1);
-      this.C[i] = this.randhclr();
-    }
-    // setInterval(() => this.draw1(), 10);     // this.draw();
-    this.drawBoard(this.canvas, this.context);
-  }
+    const gradBaseTexture = new PIXI.BaseTexture(new GradientResource());
+    // Here you can fake baseTexture size to avoid resizing all sprites
+    // There can be multiple baseTextures per gradient, but be careful:
+    // resource will spawn more glTextures!
+    gradBaseTexture.setSize(1200, 60);
 
-  drawBoard(cnvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
-    const bw = 400;
-    const bh = 400;
-    const p = 8;
-    const cw = bw + (p * 2) + 1;
-    const ch = bh + (p * 2) + 1;
+    const gradTexture = new PIXI.Texture(gradBaseTexture);
 
-    for (let x = 0; x <= bw; x += 40) {
-        context.moveTo(0.5 + x + p, p);
-        context.lineTo(0.5 + x + p, bh + p);
-    }
+    const sprite = new PIXI.Sprite(gradTexture);
+    sprite.position.set(0, 0);
+    //sprite.rotation = Math.PI / 8;
+    this.app.stage.addChild(sprite);
 
-    for (let x = 0; x <= bh; x += 40) {
-        context.moveTo(p, 0.5 + x + p);
-        context.lineTo(bw + p, 0.5 + x + p);
-    }
-
-    context.strokeStyle = 'white';
-    context.stroke();
-}
-
-
-  private draw1() {
-    for (let i = 0; i < 32; i++) {
-      const index = this.randgp(500);
-      this.X[index] = this.X[index] - (this.randgp(8) - 4);
-      this.Y[index] = this.Y[index] - (this.randgp(8) - 4);
-    }
-    this.pVoronoiD();
-  }
-  /**
-   * Draws something using the context we obtained earlier on
-   */
-  private draw() {
-    this.context.font = '30px Arial';
-    this.context.textBaseline = 'middle';
-    this.context.textAlign = 'center';
-
-    const x1 = (this.canvasEl.nativeElement as HTMLCanvasElement).width / 2;
-    const y1 = (this.canvasEl.nativeElement as HTMLCanvasElement).height / 2;
-    this.context.fillText('@realappie', x1, y1);
-
-    const canvas = this.canvasEl.nativeElement as HTMLCanvasElement;
-    const stars = 500;
-    const colorrange = [0, 60, 240];
-    for (let i = 0; i < stars; i++) {
-      const x = Math.random() * canvas.offsetWidth;
-      const y = Math.random() * canvas.offsetHeight;
-      const radius = Math.random() * 1.2;
-      const hue = colorrange[this.getRandom(0, colorrange.length - 1)];
-      const sat = this.getRandom(50, 100);
-      this.context.beginPath();
-      this.context.arc(x, y, radius, 0, 360);
-      this.context.fillStyle = 'hsl(' + hue + ', ' + sat + '%, 88%)';
-      this.context.fill();
-    }
-  }
-
-open() {
-    window.open('https://github.com/realappie', '_blank');
-  }
-
-getRandom(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-randgp(max: number) {
-    return Math.floor(Math.random() * max);
-  }
-
-randhclr() {
-    /*
-    return (
-      '#' +
-      ('00' + this.randgp(256).toString(16)).slice(-2) +
-      ('00' + this.randgp(256).toString(16)).slice(-2) +
-      ('00' + this.randgp(256).toString(16)).slice(-2)
-    );
-    */
-   return (
-    '#' +
-    ('00' + this.randgp(256).toString(16)).slice(-2) +
-    ('00') +
-    ('00')
-  );
-  }
-
-Metric(x, y, mt) {
-    if (mt === 1) {
-      return Math.sqrt(x * x + y * y);
-    }
-    if (mt === 2) {
-      return Math.abs(x) + Math.abs(y);
-    }
-    if (mt === 3) {
-      return Math.pow(
-        Math.pow(Math.abs(x), 3) + Math.pow(Math.abs(y), 3),
-        0.33333
-      );
-    }
-  }
-
-pVoronoiD() {
-    const cvs = this.canvasEl.nativeElement as HTMLCanvasElement;
-    const ctx = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext(
-      '2d'
-    );
-
-    const w = cvs.width;
-    const h = cvs.height;
-    let x = 0;
-    let y = 0;
-    let d = 0;
-    let dm = 0;
-    let j = 0;
-
-    const w1 = w - 2;
-    const h1 = h - 2;
-    const n = 500;
-    const mt = 1;
-
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, w, h);
-
-    for (y = 0; y < h1; y++) {
-      for (x = 0; x < w1; x++) {
-        dm = this.Metric(h1, w1, mt);
-        j = -1;
-        for (let i = 0; i < n; i++) {
-          d = this.Metric(this.X[i] - x, this.Y[i] - y, mt);
-          if (d < dm) {
-            dm = d;
-            j = i;
-          }
-        }
-        ctx.fillStyle = this.C[j];
-        ctx.fillRect(x, y, 1, 1);
-      }
-    }
-    ctx.fillStyle = 'black';
-    for (let i = 0; i < n; i++) {
-      ctx.fillRect(this.X[i], this.Y[i], 3, 3);
-    }
+    const basicText = new PIXI.Text('Cyberdyme');
+    basicText.x = 4;
+    basicText.y = 8;
+    this.app.stage.addChild(basicText);
   }
 }
-
-/*
-
-<!-- VoronoiD.html -->
-https://rosettacode.org/wiki/Voronoi_diagram#Version_.232.
-<html>
-<head><title>Voronoi diagram</title>
-<script>
-// HF#1 Like in PARI/GP: return random number 0..max-1
-function randgp(max) {return Math.floor(Math.random()*max)}
-// HF#2 Random hex color
-function randhclr() {
-  return "#"+
-  ("00"+randgp(256).toString(16)).slice(-2)+
-  ("00"+randgp(256).toString(16)).slice(-2)+
-  ("00"+randgp(256).toString(16)).slice(-2)
-}
-// HF#3 Metrics: Euclidean, Manhattan and Minkovski 3/20/17
-function Metric(x,y,mt) {
-  if(mt==1) {return Math.sqrt(x*x + y*y)}
-  if(mt==2) {return Math.abs(x) + Math.abs(y)}
-  if(mt==3) {return(Math.pow(Math.pow(Math.abs(x),3) + Math.pow(Math.abs(y),3),0.33333))}
-}
-// Plotting Voronoi diagram. aev 3/10/17
-function pVoronoiD() {
-  var cvs=document.getElementById("cvsId");
-  var ctx=cvs.getContext("2d");
-  var w=cvs.width, h=cvs.height;
-  var x=y=d=dm=j=0, w1=w-2, h1=h-2;
-  var n=document.getElementById("sites").value;
-  var mt=document.getElementById("mt").value;
-  var X=new Array(n), Y=new Array(n), C=new Array(n);
-  ctx.fillStyle="white"; ctx.fillRect(0,0,w,h);
-  for(var i=0; i<n; i++) {
-    X[i]=randgp(w1); Y[i]=randgp(h1); C[i]=randhclr();
-  }
-  for(y=0; y<h1; y++) {
-    for(x=0; x<w1; x++) {
-      dm=Metric(h1,w1,mt); j=-1;
-      for(var i=0; i<n; i++) {
-        d=Metric(X[i]-x,Y[i]-y,mt)
-        if(d<dm) {dm=d; j=i;}
-      }//fend i
-      ctx.fillStyle=C[j]; ctx.fillRect(x,y,1,1);
-    }//fend x
-  }//fend y
-  ctx.fillStyle="black";
-  for(var i=0; i<n; i++) {
-    ctx.fillRect(X[i],Y[i],3,3);
-  }
-}
-</script></head>
-<body style="font-family: arial, helvatica, sans-serif;">
-  <b>Please input number of sites: </b>
-  <input id="sites" value=100 type="number" min="10" max="150" size="3">&nbsp;&nbsp;
-  <b>Metric: </b>
-  <select id="mt">
-    <option value=1 selected>Euclidean</option>
-    <option value=2>Manhattan</option>
-    <option value=3>Minkovski</option>
-  </select>&nbsp;
-  <input type="button" value="Plot it!" onclick="pVoronoiD();">&nbsp;&nbsp;
-  <h3>Voronoi diagram</h3>
-  <canvas id="cvsId" width="640" height="640" style="border: 2px inset;"></canvas>
-</body>
-</html>
-
-
-
-*/
